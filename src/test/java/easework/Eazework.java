7a6864cc1ced08d10fa5afe4b16eed7e711a3db1
Shilakha <shilakha.shilakha@evry.com>
2017-10-16 15:07:07 +0530

    hh

--- src/test/java/easework/Eazework.java
+++ src/test/java/easework/Eazework.java
@@ -1,9 +1,13 @@
+package easework;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Eazework {
-	public static void main(String []args) throws InterruptedException {
+	
+	@Test
+	public void logIn() throws InterruptedException{
 		   System.setProperty("webdriver.chrome.driver", "D:\\SOFTWARES\\New folder\\Drivers\\chromedriver_win32\\chromedriver.exe");
 		   WebDriver driver = new ChromeDriver();
 		   driver.get("https://www.eazework.com/login/");
@@ -12,7 +16,7 @@ public class Eazework {
 		   Thread.sleep(1000);
 		   driver.findElement(By.xpath(".//input[@id='Login']")).sendKeys("shilakha");
 		   Thread.sleep(1000);
-		   driver.findElement(By.xpath(".//input[@id='Password']")).sendKeys("Hello@12");
+		   driver.findElement(By.xpath(".//input[@id='Password']")).sendKeys("Hello@123");
 		   Thread.sleep(1000);
 		   driver.findElement(By.xpath(".//input[@value='Login']")).click();
 		  // driver.switchTo().alert().accept();
